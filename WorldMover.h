83a15edd9d4c501c14d32393b2a879926ded8510
Daniel Sega Mattos <danielsega2003@hotmail.com>
2014-05-05 18:53:30 -0400

    First commit

---
+++ WorldMover.h
@@ -0,0 +1,15 @@
+#ifndef WORLDMOVER_H_
+#define WORLDMOVER_H_
+
+class WorldMover
+{
+public:
+	WorldMover(float XCoor) 
+		: x (XCoor){};
+
+public:
+	float x;
+private:
+};//End of World mover
+
+#endif // !WORLDMOVER_H_
